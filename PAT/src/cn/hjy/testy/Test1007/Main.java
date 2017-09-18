3cbaef2ca16ceaea6ef6681cec884f51d9faab9b
za30312 <543363559@qq.com>
2017-09-18 21:14:18 +0800

    0918

--- PAT/src/cn/hjy/testy/Test1007/Main.java
+++ PAT/src/cn/hjy/testy/Test1007/Main.java
@@ -1,14 +1,37 @@
 package cn.hjy.testy.Test1007;
 
-import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
+    static int n;
+    static int ans = 0;
+    static List<Integer> list = new ArrayList<>();
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        BigInteger a = sc.nextBigInteger();
-        int b = sc.nextInt();
-        System.out.println(a.divide(BigInteger.valueOf(b)) + " "
-                + a.mod(BigInteger.valueOf(b)));
+        n = sc.nextInt();
+
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                list.add(i);
+            }
+        }
+
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i) - list.get(i - 1) == 2) {
+                ans++;
+            }
+        }
+        System.out.println(ans);
     }
+
+    public static boolean isPrime(int n) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
